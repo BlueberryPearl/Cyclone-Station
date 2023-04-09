45b5770a59fac5ee2325c502ca1f32e7b90e828a
Blueberry Pearl <120589100+BlueberryPearl@users.noreply.github.com>
2023-04-09 18:51:08 +0100

    Added logo to Navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Link, NavLink } from "react-router-dom";
 import './Navbar.css';
+import Logo from './logo.png'
+
+
 export default function Navbar() {
     return (
         <nav className="navbar">
             <ul className="nav-pages">
-                <li className="nav-home">
+                <li><NavLink to="/"><img className="logo" src={Logo}></img></NavLink></li>
+                <li className="li-home">
                     <NavLink className="NavLink NavLink-home" to="/Home">Home</NavLink>
                 </li>
-                <li>
+                <li className="li-savedThemes">
                     <NavLink className="NavLink" to="/SavedThemes">Saved Themes</NavLink>
                 </li>
             </ul>
