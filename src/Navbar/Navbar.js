0958a2e542d8c73b4f53418fb3984a04f9bd1ed9
Blueberry Pearl <120589100+BlueberryPearl@users.noreply.github.com>
2023-05-02 12:55:54 +0100

    Added media query to Navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import './Navbar.css';
 import Logo from './logo.png'
+import { ReactComponent as Home } from "./house-solid.svg";
+import { ReactComponent as Saved } from "./heart-solid.svg";
 
 
 export default function Navbar() {
@@ -10,10 +12,15 @@ export default function Navbar() {
             <ul className="nav-pages">
                 <li><NavLink to="/"><img className="logo" alt="Logo" src={Logo}></img></NavLink></li>
                 <li className="li-home">
-                    <NavLink className="NavLink NavLink-home" to="/Home">Home</NavLink>
+                    <NavLink className="NavLink NavLink-home" to="/Home"><p className="menu-text">Home</p><Home className="home-icon" /></NavLink>
                 </li>
                 <li className="li-savedThemes">
-                    <NavLink className="NavLink" to="/SavedThemes">Saved Themes</NavLink>
+                    <NavLink className="NavLink" to="/SavedThemes"><p className="menu-text">Saved Themes</p><Saved className="saved-icon" /></NavLink>
+                </li>
+                <li className="menu">
+                    <div className="menu-circle"></div>
+                    <div className="menu-circle"></div>
+                    <div className="menu-circle"></div>
                 </li>
             </ul>
             <div className="nav-buttons">
